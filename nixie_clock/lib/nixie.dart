5cc3d7cf4c0c2fd7e2d148961e3179138a37a722
Nikita Goncharuk <nikitago2000@gmail.com>
2020-01-20 16:40:48 +0700

    [ADD] License.

--- nixie_clock/lib/nixie.dart
+++ nixie_clock/lib/nixie.dart
@@ -1,3 +1,7 @@
+// Copyright 2020 Nikita Goncharuk. All rights reserved.
+// Use of this source code is governed by a MIT license that can be
+// found in the LICENSE file.
+
 import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
--- nixie_clock/lib/nixie_clock.dart
+++ nixie_clock/lib/nixie_clock.dart
@@ -1,5 +1,5 @@
-// Copyright 2019 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
+// Copyright 2020 Nikita Goncharuk. All rights reserved.
+// Use of this source code is governed by a MIT license that can be
 // found in the LICENSE file.
 
 import 'dart:async';
